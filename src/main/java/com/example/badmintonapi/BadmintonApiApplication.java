4cc03c51f2988da36247a13bf550fe063925caff
kuanglingxi <1148273143@qq.com>
2020-01-31 17:24:51 +0800

    init commit

---
+++ src/main/java/com/example/badmintonapi/BadmintonApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.badmintonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadmintonApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BadmintonApiApplication.class, args);
+    }
+
+}
